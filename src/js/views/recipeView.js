247826558d1b198d3ba6649b7674fd2a2b152211
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2023-06-06 19:24:13 -0700

    changed to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,8 +2,8 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
-// import fracty from 'fracty';
+// import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -153,7 +153,7 @@ class RecipeView extends View {
               </svg>
               <div class="recipe__quantity">${
                 ingredient.quantity
-                  ? new Fraction(ingredient.quantity).toString()
+                  ? new fracty(ingredient.quantity).toString()
                   : ''
               }</div>
               <div class="recipe__description">
